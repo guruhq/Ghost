1e03f22627e46ad26f78e671e1aac3a0c3399c37
Pete Michel <peterman225@gmail.com>
2015-01-12 15:34:08 -0500

    Using ghost-s3-file-storage

--- core/server/storage/index.js
+++ core/server/storage/index.js
@@ -1,24 +1,26 @@
 var errors  = require('../errors'),
-    storage = {};
+config  = require('../config'),
+storage = {}; 
 
 function getStorage(storageChoice) {
     // TODO: this is where the check for storage apps should go
     // Local file system is the default.  Fow now that is all we support.
-    storageChoice = 'local-file-store';
+    storageChoice = config.fileStore;
 
     if (storage[storageChoice]) {
         return storage[storageChoice];
-    }
+    }   
 
     try {
         // TODO: determine if storage has all the necessary methods.
-      storage[storageChoice] = require('ghost-s3')({
-        errors: errors,
-        config: require('../config')().aws
-      });
+        storage[storageChoice] = require('./' + storageChoice);
     } catch (e) {
-        errors.logError(e);
-    }
+        try {
+            storage[storageChoice] = require(storageChoice);
+        } catch (e) {
+            errors.logError(e);
+        }   
+    }   
 
     // Instantiate and cache the storage module instance.
     storage[storageChoice] = new storage[storageChoice]();
