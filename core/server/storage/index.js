0d0939a80b7bff8885ce55430174e4e3734df338
Pete Michel <peterman225@gmail.com>
2015-01-14 09:23:40 -0500

    Updating 0.5.8 to use s3 storage

--- core/server/storage/index.js
+++ core/server/storage/index.js
@@ -1,21 +1,26 @@
 var errors  = require('../errors'),
-    storage = {};
+config  = require('../config'),
+storage = {}; 
 
 function getStorage(storageChoice) {
     // TODO: this is where the check for storage apps should go
     // Local file system is the default.  Fow now that is all we support.
-    storageChoice = 'local-file-store';
+    storageChoice = config.fileStore;
 
     if (storage[storageChoice]) {
         return storage[storageChoice];
-    }
+    }   
 
     try {
         // TODO: determine if storage has all the necessary methods.
         storage[storageChoice] = require('./' + storageChoice);
     } catch (e) {
-        errors.logError(e);
-    }
+        try {
+            storage[storageChoice] = require(storageChoice);
+        } catch (e) {
+            errors.logError(e);
+        }   
+    }   
 
     // Instantiate and cache the storage module instance.
     storage[storageChoice] = new storage[storageChoice]();
